3df2b19257f6dfcdbd27e080b6dbd0b87b59a166
caarloshenrique <carlos.silva@novaandradina.org>
2020-05-19 15:01:21 -0300

    initial commit

---
+++ src/br/com/executable/Executavel.java
@@ -0,0 +1,10 @@
+package br.com.executable;
+
+import br.com.view.FrmLogin;
+
+public class Executavel {
+
+    public static void main(String[] args) {
+        new FrmLogin().setVisible(true);
+    }
+}
